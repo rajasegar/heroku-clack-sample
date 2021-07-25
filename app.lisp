6802cd2d47c5e1278825d23487e9e22914eb7bfb
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-07-25 12:40:05 +0530

    chore: add djula templates

--- app.lisp
+++ app.lisp
@@ -1,3 +1,28 @@
+(ql:quickload '(:djula))
+
+(djula:add-template-directory  #P"templates/")
+
+(defparameter +index.html+ (djula:compile-template* "index.html"))
+
+(defparameter +about.html+ (djula:compile-template* "about.html"))
+
+(defparameter +contact.html+ (djula:compile-template* "contact.html"))
+
+(defun render (template)
+  (let ((html (make-string-output-stream)))
+    (djula:render-template* template html)
+    `(200 (:content-type "text/html")
+          (,(format nil "~a" (get-output-stream-string html))))))
+
+
 (lambda (env)
-  (declare (ignore env))
-  '(200 (:content-type "text/plain") ("Hello, Clack!")))
+  (cond
+    ;; about page
+    ((string= "/about" (getf env :request-uri))
+     (render +about.html+))
+    ;; contact page
+    ((string= "/contact" (getf env :request-uri))
+     (render +contact.html+))
+    ;; default route
+    (t
+     (render +index.html+))))
